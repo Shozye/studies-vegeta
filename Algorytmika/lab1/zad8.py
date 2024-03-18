444614ebde8151ab5e9eae441de7fd3f47cb685f
base <Mateusz P>
2024-03-18 11:22:22 +0100

    done

--- Algorytmika/lab1/zad8.py
+++ Algorytmika/lab1/zad8.py
@@ -1,35 +1,12 @@
-import random
-import math
-from matplotlib import pyplot as plt
 
-def monte_carlo(sample: int=1000) -> float:
-    
-    counter: int = 0
-    for _ in range(sample):
-        r1 = random.random()*math.pi
-        r2 = random.random()
-        
-        if r2 < math.sin(r1):
-            counter += 1
-    
-    answer = (counter / sample) * math.pi
-    #print(f"Approximately: {sample=}, {counter=}, {answer=}")
-    return answer
-    
-def error(actual: float, expected: float) -> float:
-    return abs(actual - expected) / expected
+def lcs(x: str, y: str) -> int:
+    # to jakis dp jest. dp[0,0] = x[0] == y[0]
+    # dp[n+1,m+1] = 1 + dp[n][m] if x[n] == x[m]
+    # dp[n+1, m+1] = max(dp[n, m+1], dp[n+1, m])
+    dp = [[0 for j in range(len(y)+1)] for i in range(len(x)+1)]
 
-def avg(x: list[float]) -> float:
-    return sum(x)/len(x)
-
-def main():
-    X = list(range(10,10001, 10))
-    Y: list[float] = []
-    for x in X:
-        Y.append(avg([error(monte_carlo(x), 2) for _ in range(10)]))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            dp[i+1][j+1] = 1+dp[i][j]
     
-    plt.plot(X, Y) # type: ignore
-    plt.savefig("zad8.png") # type: ignore
     
-if __name__ == "__main__":
-    main()
