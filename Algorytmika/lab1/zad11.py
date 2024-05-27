89d11fcfc58d574c3eb4afdfda66f6968f173f19
base <Mateusz Pelechaty>
2024-05-27 14:53:17 +0200

    changed namings

--- Algorytmika/lab1/zad11.py
+++ Algorytmika/lab1/zad11.py
@@ -1,65 +1,35 @@
-from copy import deepcopy
 import random
+import math
+from matplotlib import pyplot as plt
 
-class Graph:
-    graph: dict[int, list[int]]
+def monte_carlo(sample: int=1000) -> float:
     
-    def __init__(self, graph: dict[int, list[int]]) -> None:
-        self.graph = graph
-        self.highest_node = 0
-    
-    def clone(self):
-        return Graph(deepcopy(self.graph))
-    
-    def add_node(self, u: int):
-        self.graph[u] = list()
-        if u > self.highest_node:
-            self.highest_node = u
+    counter: int = 0
+    for _ in range(sample):
+        r1 = random.random()*math.pi
+        r2 = random.random()
         
-    def get_amount_edges(self) -> int:
-        amount_edges = 0
-        for edges in self.graph.values():
-            amount_edges += len(edges)
-        return amount_edges
+        if r2 < math.sin(r1):
+            counter += 1
     
-    def add_edge(self, u: int, v: int):
-        self.graph[u].append(v)
+    answer = (counter / sample) * math.pi
+    #print(f"Approximately: {sample=}, {counter=}, {answer=}")
+    return answer
     
-    def contract_edge(self, u: int, v: int):
-        edges_of_uv: list[int] = list()
-        for e in self.graph[u]:
-            if e != v:
-                edges_of_uv.append(e)
-        for e in self.graph[v]:
-            if e != u:
-                edges_of_uv.append(e)
-        
-        uv_node: int = self.highest_node+1
-        self.highest_node += 1
-        del self.graph[u]
-        del self.graph[v]
-        for edges in self.graph.values():
-            for i, target in enumerate(edges):
-                if target == u or target == v:
-                    edges[i] = uv_node
-        
-        self.graph[uv_node] = edges_of_uv
-    
-    def get_random_edge(self) -> tuple[int, int]:
-        amount_edges: int = self.get_amount_edges()
-        random_edge_index: int = random.randrange(0, amount_edges)
-        for u, edges in self.graph.items():
-            if len(edges) < random_edge_index:
-                return (u, edges[random_edge_index])
-            random_edge_index -= len(edges)
+def error(actual: float, expected: float) -> float:
+    return abs(actual - expected) / expected
 
-        raise Exception("?")
+def avg(x: list[float]) -> float:
+    return sum(x)/len(x)
 
-
-def karger_min_cut(g: Graph) -> int:
-    graph = g.clone()
-    while len(graph.graph) > 2:
-        u, v = graph.get_random_edge()
-        graph.contract_edge(u, v)
-    return len(list(graph.graph.values())[0])
+def main():
+    X = list(range(10,10001, 10))
+    Y: list[float] = []
+    for x in X:
+        Y.append(avg([error(monte_carlo(x), 2) for _ in range(10)]))
     
+    plt.plot(X, Y) # type: ignore
+    plt.savefig("zad10.png") # type: ignore
+    
+if __name__ == "__main__":
+    main()
