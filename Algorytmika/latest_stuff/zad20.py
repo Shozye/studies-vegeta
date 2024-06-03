aa91656afba9fc484e6517955dad73b75ecf1aeb
sanaa139 <sandra.szwed19@gmail.com>
2024-06-03 13:39:16 +0200

    commit finished work on pwr

---
+++ Algorytmika/latest_stuff/zad20.py
@@ -0,0 +1,12 @@
+# Ćwiczenie 20
+# Znajdź bibliotekę swojego ulubionego języka programowania która zawiera implementację funkcji MurMurHash. 
+# Zastosuj ją do konstrukcji funkcji haszującej h_s w wartościach w zbiorze {0, . . . , 20} dla ziarna s. 
+# Zastosuj ją dla ciągu wyrazów ulubionej książki, którą masz zapisaną w formacie ASCII.
+# 1. Zbadaj histogram otrzymanych wartości dla kilku różnych wartości ziarna.
+# 2. Ustal dwa różne słowa a i b i przetestuj na tych słowach liczbę kolizji h_s(a) = hs(b) dla losowo wybranych 1000 wartości ziarna s
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
